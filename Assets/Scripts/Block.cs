b4cc6b55e1e67bcc8d2383f38d70c27c980d3439
Taha <t2905j@gmail.com>
2021-02-07 17:42:55 +0530

    Added the game panel

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = (string)gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject m_frontpanel;
+    public GameObject m_gamePanel;
+
+    public void Play()
+    {
+        m_frontpanel.SetActive(false);
+        m_gamePanel.SetActive(true);
+    }
+}
